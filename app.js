45daf5f347a55649edad2ce6734efc509284190c
Himanshu <78425898+astroboyhimanshu@users.noreply.github.com>
2023-07-08 01:30:49 +0530

    Update app.js
    MongoDB code has been added to save the contact info to a NoSQL Database (MongoDB)

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
+
+
+// getting-started.js
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser")
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+
+
+
 const port = 8000;
+
+
+// Define Mongoose Schema 
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String
+
+  });
+
+const Contact = mongoose.model('Contact', contactSchema);
+
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
@@ -21,6 +49,19 @@ app.get('/contact', (req, res)=>{
     const params = {}
     res.status(200).render('contact.pug', params);
 })
+
+
+app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved to the DataBase")
+    }).catch(()=>{
+        res.status(400).send("Item was not sent to Db")
+    });
+
+    // res.status(200).render('contact.pug');
+})
+
 
 
 // START THE SERVER
